115193c6a311e2ff1560a652b5898e130096ceee
Guilherme BONFADA <guilhermebonfada@gmail.com>
2020-06-18 12:10:48 -0300

    Adicionei autofac para o mvc

---
+++ Contratos.App/App_Start/IocConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Contratos.Ioc;
+using System.Web.Mvc;
+
+namespace Contratos.App.App_Start
+{
+    public static class IocConfiguration
+    {
+        public static void ConfigureDependencyInjection()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            BootStrapper.RegisterServices(builder);
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
