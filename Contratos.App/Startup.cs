f30657b4864b062de9ff61a37248f2d03b9979be
Guilherme BONFADA <guilhermebonfada@gmail.com>
2020-06-18 11:46:14 -0300

    instalação do Owin para login

---
+++ Contratos.App/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System.Web.Helpers;
+
+[assembly: OwinStartup(typeof(Contratos.App.Startup))]
+
+namespace Contratos.App
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Autenticacao/Login")
+            });
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = "Login";
+        }
+    }
+}
