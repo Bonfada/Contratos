67a43b4cf89b4d9590af54f32fbed6ce47294849
Guilherme BONFADA <guilhermebonfada@gmail.com>
2020-06-18 22:14:06 -0300

    Customização do Menu do usuário Logado.

---
+++ Contratos.App/Controllers/PerfilUsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Contratos.App.ViewModels;
+using System.Web.Mvc;
+
+namespace Contratos.App.Controllers
+{
+    public class PerfilUsuarioController : Controller
+    {
+        [Authorize]
+        public ActionResult AlterarSenha()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AlterarSenha(PerfilUsuarioViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            TempData["Mensagem"] = "Senha alterada com sucesso.";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
